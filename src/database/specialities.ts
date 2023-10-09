59b7b9539b7ee9a345e755839592ddea0cca3034
Guilherme <evenciodev@gmail.com>
2023-10-09 13:47:09 -0300

    update: abstract texts in specialities file.

--- src/database/specialities.ts
+++ src/database/specialities.ts
@@ -23,7 +23,7 @@ export interface Speciality {
 
 export const specalities: Speciality[] = [
   {
-    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Placerat in egestas erat imperdiet sed euismod nisi. Mi in nulla posuere sollicitudin aliquam. Aenean et tortor at risus viverra adipiscing at in. Ipsum faucibus vitae aliquet nec. Id diam vel quam elementum pulvinar etiam. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Pretium aenean pharetra magna ac placerat vestibulum lectus. Vitae semper quis lectus nulla at. Aenean euismod elementum nisi quis eleifend quam adipiscing.',
+    abstract: 'Você sabe quais são os seus direitos como consumidor? Você já se sentiu lesado por alguma empresa ou prestador de serviço? Se você precisa de orientação ou defesa em casos de cobranças indevidas, empréstimos fraudulentos, revisional de financiamentos com juros abusivos, defeitos em produtos ou serviços, propaganda enganosa, cancelamento de contrato, entre outros, conte com a nossa ajuda especializada em Direito do Consumidor. Nós vamos lutar pelos seus direitos e buscar a melhor solução para o seu problema.',
     waysToHelp: [
       {
         title: 'maneira 1',
@@ -44,7 +44,7 @@ export const specalities: Speciality[] = [
     link: '/direito-do-consumidor'
   },
   {
-    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Placerat in egestas erat imperdiet sed euismod nisi. Mi in nulla posuere sollicitudin aliquam. Aenean et tortor at risus viverra adipiscing at in. Ipsum faucibus vitae aliquet nec. Id diam vel quam elementum pulvinar etiam. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Pretium aenean pharetra magna ac placerat vestibulum lectus. Vitae semper quis lectus nulla at. Aenean euismod elementum nisi quis eleifend quam adipiscing.',
+    abstract: 'A Previdência Social é um direito de todos os trabalhadores que contribuem para o sistema. Mas nem sempre é fácil garantir os benefícios a que se tem direito, como aposentadoria, pensão por morte, auxílio-doença, salário-maternidade, entre outros. Por isso, é importante contar com o apoio de um advogado que conhece as leis e as normas que regem o assunto. Nós podemos te ajudar a requerer, revisar ou restabelecer o seu benefício previdenciário, de forma rápida e segura.',
     waysToHelp: [
       {
         title: 'maneira 1',
@@ -61,11 +61,11 @@ export const specalities: Speciality[] = [
     ],
     description: 'Ea quidem vitae est galisum animi qui eligendi autem est adipisci fugiat eum rerum tempore.',
     id: 2,
-    name: 'Direito do Consumidor',
-    link: '/direito-do-consumidor'
+    name: 'Direito Previdenciário',
+    link: '/direito-previdenciario'
   },
   {
-    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Placerat in egestas erat imperdiet sed euismod nisi. Mi in nulla posuere sollicitudin aliquam. Aenean et tortor at risus viverra adipiscing at in. Ipsum faucibus vitae aliquet nec. Id diam vel quam elementum pulvinar etiam. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Pretium aenean pharetra magna ac placerat vestibulum lectus. Vitae semper quis lectus nulla at. Aenean euismod elementum nisi quis eleifend quam adipiscing.',
+    abstract: 'O Direito Cível é o ramo do direito que trata das relações entre as pessoas, tanto físicas quanto jurídicas. Ele abrange diversas áreas, como contratos, responsabilidade civil, propriedade, família, sucessões, entre outras. Se você precisa de assessoria ou representação em questões cíveis, como cobranças, indenizações, inventários, divórcios, reconhecimento de paternidade, entre outras, você pode contar conosco. Nós vamos defender os seus interesses e buscar a melhor solução para o seu caso.',
     waysToHelp: [
       {
         title: 'maneira 1',
@@ -82,11 +82,11 @@ export const specalities: Speciality[] = [
     ],
     description: 'Ea quidem vitae est galisum animi qui eligendi autem est adipisci fugiat eum rerum tempore.',
     id: 3,
-    name: 'Direito do Consumidor',
-    link: '/direito-do-consumidor'
+    name: 'Direito Civil',
+    link: '/direito-civil'
   },
   {
-    abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus pulvinar elementum integer enim. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Placerat in egestas erat imperdiet sed euismod nisi. Mi in nulla posuere sollicitudin aliquam. Aenean et tortor at risus viverra adipiscing at in. Ipsum faucibus vitae aliquet nec. Id diam vel quam elementum pulvinar etiam. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Pretium aenean pharetra magna ac placerat vestibulum lectus. Vitae semper quis lectus nulla at. Aenean euismod elementum nisi quis eleifend quam adipiscing.',
+    abstract: 'O Direito Trabalhista é o ramo do direito que regula as relações entre empregados e empregadores. Ele visa garantir os direitos e deveres de ambas as partes, bem como a harmonia e a justiça no ambiente de trabalho. Se você precisa de orientação ou defesa em questões trabalhistas, como rescisão contratual, verbas rescisórias, horas extras, férias, décimo terceiro salário, adicional noturno, insalubridade, periculosidade, assédio moral ou sexual, entre outras, você pode contar também com essa nossa especialidade em Direito Trabalhista. Nós vamos defender os seus direitos e buscar a melhor solução para o seu caso.',
     waysToHelp: [
       {
         title: 'maneira 1',
@@ -103,7 +103,7 @@ export const specalities: Speciality[] = [
     ],
     description: 'Ea quidem vitae est galisum animi qui eligendi autem est adipisci fugiat eum rerum tempore.',
     id: 4,
-    name: 'Direito do Consumidor',
-    link: '/direito-do-consumidor'
+    name: 'Direito Trabalhista',
+    link: '/direito-trabalhista'
   }
 ]
